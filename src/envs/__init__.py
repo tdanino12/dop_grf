0ec5632370f16329570ccaf4a53dcbd84ef4abd3
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-08-11 18:08:22 +0300

    Update __init__.py

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -6,7 +6,15 @@ import os
 def env_fn(env, **kwargs) -> MultiAgentEnv:
     return env(**kwargs)
 
-REGISTRY = {}
+REGISTRY = {
+    # "sc2": partial(env_fn, env=StarCraft2Env),
+    # "matrix_game_1": partial(env_fn, env=Matrix_game1Env),
+    # "matrix_game_2": partial(env_fn, env=Matrix_game2Env),
+    # "matrix_game_3": partial(env_fn, env=Matrix_game3Env),
+    # "mmdp_game_1": partial(env_fn, env=mmdp_game1Env)
+    "academy_3_vs_1_with_keeper": partial(env_fn, env=Academy_3_vs_1_with_Keeper),
+    "academy_counterattack_hard": partial(env_fn, env=Academy_Counterattack_Hard),
+}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
 
 if sys.platform == "linux":
